14cd7ac935482fc892315c38e7d29b1dd0ddc3d7
TotoDeskSlam <25SlateJ@mbbc.qld.edu.au>
2023-07-20 12:30:12 +1000

    Created Spaceship object

--- Objects/Title.py
+++ Objects/Title.py
@@ -1,4 +1,5 @@
 from GameFrame import RoomObject
+import pygame
 
 class Title(RoomObject):
     """
@@ -6,7 +7,18 @@ class Title(RoomObject):
     """
     def __init__(self, room, x, y):
         RoomObject.__init__(self, room, x, y)
-
+        
         # set image
         image = self.load_image("Title.png")
         self.set_image(image,800,350)
+        
+        # register for key events
+        self.handle_key_events = True 
+        
+    def key_pressed(self, key):
+        """
+        If the key pressed is space the game will start
+        """
+        
+        if key[pygame.K_SPACE]:
+            self.room.running = False
